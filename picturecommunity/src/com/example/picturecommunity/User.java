f0e5fc0aebf362bb2daa2c0922028707bb1b2a5b
Bronika <Bronika@gmx.de>
2015-03-27 21:26:56 +0100

    add project

---
+++ picturecommunity/src/com/example/picturecommunity/User.java
@@ -0,0 +1,30 @@
+package com.example.picturecommunity;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Long id;
+	
+	private String userName;
+	private String password;
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
